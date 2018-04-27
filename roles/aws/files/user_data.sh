9877fe5d6b806551b414bfaf043004964c9f9b09
manojk79 <31552785+manojk79@users.noreply.github.com>
2018-04-27 12:16:25 -0500

    Update user_data.sh

--- roles/aws/files/user_data.sh
+++ roles/aws/files/user_data.sh
@@ -4,17 +4,6 @@ sudo yum update -y
 sudo yum -y install httpd mod_ssl
 # Create new ssl certificate
 openssl req -newkey rsa:2048 -nodes -keyout /home/ec2-user/devops.key -x509 -days 365 -out /home/ec2-user/devops.crt -subj "/C=US/ST=Wisconsin/L=Milwaukee/O=Devops/CN=www.manoj-devops-project.com"
-cat > /var/www/html/index.html << 'EOL'
-<html>
-<head>
-<title>Hello World</title>
-</head>
-<body>
-<h1>Hello World!</h1>
-</body>
-</html>
-EOL
-cat >> /etc/httpd/conf/httpd.conf << 'EOT'
 LoadModule ssl_module /etc/httpd/modules/mod_ssl.so
 <VirtualHost *:443>
     ServerName www.manoj-devops-project.com
